5c6aee8de4bf0d949933fbc5c710634764cfa5c2
Mezekr W <mezi.edu@tuta.io>
2024-12-03 15:14:21 +0100

    refactor the navigation bar to be sticky on top

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 function toggleMobileMenu() {
 	document.getElementById('navigation-menu').classList.toggle('active');
 }
+
+const navigationHeight = document.querySelector('header').offsetHeight;
+document.documentElement.style.setProperty(
+	'--scroll-padding',
+	navigationHeight - 1 + 'px'
+);
